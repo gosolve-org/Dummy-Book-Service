532a505285d3d4da78b024b412602d69b1a8149e
Tom Daniel <tom.daniel@ae.be>
2022-06-20 23:47:02 +0200

    GS-144 - Refactor dto usage

---
+++ Book.Api/MappingProfiles/DtoMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using GoSolve.HttpClients.Dummy.Book.Contracts;
+using GoSolve.HttpClients.Dummy.Review.Contracts;
+
+namespace GoSolve.Dummy.Book.Api.MappingProfiles;
+
+public class DtoMappingProfile : Profile
+{
+    public DtoMappingProfile()
+    {
+        CreateMap<BookRequest, Business.Models.Book>();
+        CreateMap<Business.Models.Book, BookResponse>();
+        CreateMap<Business.Models.Book, DetailedBookResponse>();
+        CreateMap<ReviewResponse, DetailedBookReviewResponse>();
+        CreateMap<IEnumerable<ReviewResponse>, DetailedBookResponse>()
+            .ForMember(dest => dest.Reviews, opt => opt.MapFrom(src => src));
+    }
+}
