1acd977782ef49aff7569762d43020a268bc9e23
Tom Daniel <tom.daniel@ae.be>
2022-12-23 02:04:26 +0100

    Update pipeline extension method structure & enable Serilog logging

---
+++ Book.Api/ExtensionMethods/PipelineExtensionMethods.cs
@@ -0,0 +1,29 @@
+﻿using GoSolve.Dummy.Book.Api.MappingProfiles;
+using GoSolve.Dummy.Book.Business.ExtensionMethods;
+using GoSolve.HttpClients.Dummy.Review;
+using GoSolve.Tools.Api.ExtensionMethods;
+using GoSolve.Tools.Common.HttpClients;
+
+namespace GoSolve.Dummy.Book.Api.ExtensionMethods;
+
+public static class PipelineExtensionMethods
+{
+    public static IServiceCollection AddApiLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAutoMapper(typeof(DtoMappingProfile));
+
+        services.AddInternalHttpClient<IReviewHttpClient, ReviewHttpClient>(configuration, "review");
+
+        services.AddApiTools(configuration);
+        services.AddBusinessLayer(configuration);
+
+        return services;
+    }
+
+    public static WebApplication UseApiLayer(this WebApplication app)
+    {
+        app.UseApiTools();
+
+        return app;
+    }
+}
---
+++ Book.Business/ExtensionMethods/PipelineExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using GoSolve.Dummy.Book.Business.Services;
+using GoSolve.Dummy.Book.Business.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GoSolve.Dummy.Book.Business.ExtensionMethods;
+
+public static class PipelineExtensionMethods
+{
+    public static IServiceCollection AddBusinessLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<IBookService, BookService>();
+
+        return services;
+    }
+}
