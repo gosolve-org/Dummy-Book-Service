f314b69a776db0194da5bb3334b0b352669430aa
Tom Daniel <tom.daniel@ae.be>
2022-12-31 01:07:40 +0100

    Fix seeding data datetimes

--- Book.Data/Migrations/20221231000540_FixSeedingDataDateTimes.Designer.cs
+++ Book.Data/Migrations/20221231000540_FixSeedingDataDateTimes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GoSolve.Dummy.Book.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GoSolve.Dummy.Book.Data.Migrations
 {
     [DbContext(typeof(BookDbContext))]
-    partial class BookDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221231000540_FixSeedingDataDateTimes")]
+    partial class FixSeedingDataDateTimes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
